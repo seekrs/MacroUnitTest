6a466c862106ef02ccd2b303f1d3d147fc622cb4
Kbz-8 <kbz_8.code@proton.me>
2024-12-20 01:39:21 +0100

    adding base test

---
+++ Runtime/Includes/Tests/Enums.h
@@ -0,0 +1,31 @@
+#ifndef MLX_UT_TESTS_ENUMS
+#define MLX_UT_TESTS_ENUMS
+
+#include <PreCompiled.h>
+
+namespace mlxut
+{
+	enum class TestState
+	{
+		Pending = 0,
+		Running,
+		Success,
+		Fail,
+
+		EndEnum
+	};
+	constexpr std::size_t TestStateCount = static_cast<std::size_t>(TestState::EndEnum);
+
+	enum class TestDiffResult
+	{
+		Unprocessed = 0,
+		Negligible,
+		Acceptable,
+		Unacceptable,
+
+		EndEnum
+	};
+	constexpr std::size_t TestDiffResult = static_cast<std::size_t>(TestDiffResult::EndEnum);
+}
+
+#endif
---
+++ Runtime/Includes/Tests/Tester.h
@@ -0,0 +1,10 @@
+#ifndef MLX_UT_TESTER
+#define MLX_UT_TESTER
+
+#include <PreCompiled.h>
+
+namespace mlxut
+{
+}
+
+#endif
